86d44ac8c713a4dd47fc1fa1e7146b9bc7c2d003
noureqo <128331928+noureqo@users.noreply.github.com>
2025-03-07 23:34:44 +0200

    0.1-polished

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const authMiddleware = require('./middleware/auth');
 
-// Routes
 const booksRouter = require('./routes/books');
 const authorsRouter = require('./routes/authors');
 const transactionsRouter = require('./routes/transactions');
 
-// Middleware
-app.use(bodyParser.json());
 app.use(express.json());
 app.use(authMiddleware);
 
-// Route handlers
 app.use('/api/books', booksRouter);
 app.use('/api/authors', authorsRouter);
 app.use('/api/transactions', transactionsRouter);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
